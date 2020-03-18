9ca6a8a5c33846e27e7ad4e15dfc0831fa8f43c8
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-18 21:05:13 +0100

    added toast cmp

--- aura/FW_ProductsSearchResults/FW_ProductsSearchResultsController.js
+++ aura/FW_ProductsSearchResults/FW_ProductsSearchResultsController.js
@@ -1,6 +1,5 @@
 ({
     onInit: function(component, event, helper){
-        console.log('doInit zdjecia');
         let meta = document.createElement("meta");
         meta.setAttribute("name", "format-detection");
         meta.setAttribute("http-equiv", "Content-Security-Policy")
@@ -12,6 +11,5 @@
     receiveSearchResults: function(component, event, helper){
         let resultsFromEvent = event.getParam("results");
         component.set("v.results", resultsFromEvent);
-        console.log('results in receiveSearchResults:'+JSON.stringify(component.get('v.results')));
     }
 })
