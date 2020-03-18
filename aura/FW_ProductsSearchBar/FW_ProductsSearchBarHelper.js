551205a004107c43679ccde0a5485d365ade88af
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-18 12:27:32 +0100

    added doSearch function in search bar helper

---
+++ aura/FW_ProductsSearchBar/FW_ProductsSearchBarHelper.js
@@ -0,0 +1,29 @@
+({
+    doSearch: function(component){
+        let action = component.get("c.getSearchResults");
+        let name = component.get("V.searchItemName");
+        action.setParams({
+            "name": name
+        });
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if(state === 'SUCCESS'){
+                let appEvent = $A.get("e.c:FW_SendProductsToProductsList");
+                appEvent.setParams({
+                    "results": response.getReturnValue()
+                });
+                appEvent.fire();
+            }else{
+                let title = $A.get("$Label.c.Error");
+                let message = $A.get("$Label.c.Error");
+                let errors = response.getError();
+                if (errors && Array.isArray(errors) && errors.length > 0) {
+                    message = errors[0].message;
+                }
+                let toastComponent = component.find('customToast');
+                toastComponent.ShowToast(title, message, 'error', 'sticky');
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})
