f19cb13b3298586ba393259c295a309bc2233e75
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-19 14:49:46 +0100

    added possibility of selecting product

--- aura/FW_ProductTile/FW_ProductTileController.js
+++ aura/FW_ProductTile/FW_ProductTileController.js
@@ -1,14 +1,21 @@
 
 ({
     onInit: function(component, event, helper){
-            let meta = document.createElement("meta");
-            meta.setAttribute("name", "format-detection");
-            meta.setAttribute("http-equiv", "Content-Security-Policy")
-            meta.setAttribute("content", "date=no;img-src 'self' data:");
-            document.getElementsByTagName('head')[0].appendChild(meta);
+        let meta = document.createElement("meta");
+        meta.setAttribute("name", "format-detection");
+        meta.setAttribute("http-equiv", "Content-Security-Policy")
+        meta.setAttribute("content", "date=no;img-src 'self' data:");
+        document.getElementsByTagName('head')[0].appendChild(meta);
     },
 
     selectItem: function(component, event, helper){
-
+        console.log('selectItem');
+        let selectedProductEvent = component.getEvent("selectedProduct");
+        let product = component.get("v.item");
+        selectedProductEvent.setParams({
+            "product": product
+        });
+        selectedProductEvent.fire();
+        console.log('after fire selectedProductEvent');
     }
 })
