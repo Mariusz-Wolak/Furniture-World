5a67632fab53c0a0c7b7c24c1cb0ff82f594752a
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-23 20:43:51 +0100

    added methods to add product to observe list

--- aura/FW_ProductView/FW_ProductViewController.js
+++ aura/FW_ProductView/FW_ProductViewController.js
@@ -1,19 +1,42 @@
 ({
+    doInit: function(component, event, helper){
+
+    }
+
     toggleObserved: function(component, event, helper){
         let observeIcon = component.find('observeIcon');
+        let productId = component.get('v.product.Id');
+        let toastComponent = component.find('customToast');
+        let toastTitle;
+        let toastMsg;
 
         if($A.util.hasClass(observeIcon, 'greyIcon')){
-            $A.util.removeClass(observeIcon, 'greyIcon');
-            $A.util.addClass(observeIcon, 'highlightedIcon');
-
-//            let title = $A.get("$Label.c.Success");
-//            let message = $A.get("$Label.c.Unknown_Error");
-//            let toastComponent = component.find('customToast');
-//            toastComponent.ShowToast('title', 'message', 'success', 'sticky');
+            let action = component.get('c.insertToObserved');
+            action.setParams({
+                "productId": productId
+            });
+            action.setCallback(this, function(response){
+                let state = response.getState();
+                if(state === 'SUCCESS'){
+                    $A.util.removeClass(observeIcon, 'greyIcon');
+                    $A.util.addClass(observeIcon, 'highlightedIcon');
+                    toastTitle = $A.get("$Label.c.Success");
+                    toastMsg = $A.get("$Label.c.Success_Adding_Item");
+                }else{
+                    toastTitle = $A.get("$Label.c.Error");
+                    toastMsg = $A.get("$Label.c.Unknown_Error");
+                    let errors = response.getError();
+                    if (errors && Array.isArray(errors) && errors.length > 0) {
+                        toastMsg = errors[0].message;
+                    }
+                }
+            });
+            $A.enqueueAction(action);
         }else{
             $A.util.removeClass(observeIcon, 'highlightedIcon');
             $A.util.addClass(observeIcon, 'greyIcon');
         }
+            toastComponent.ShowToast(toastTitle, toastMsg, 'success', 'sticky');
     },
 
     toggleBasket: function(component, event, helper){
