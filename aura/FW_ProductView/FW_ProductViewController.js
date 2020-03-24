50c65ff1ae243cee451a4b2ca5e5e56c9689a682
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-24 13:35:59 +0100

    moved product view ctrl to helper

--- aura/FW_ProductView/FW_ProductViewController.js
+++ aura/FW_ProductView/FW_ProductViewController.js
@@ -1,38 +1,13 @@
 ({
     doInit: function(component, event, helper){
-        helper.returnNewestProducts(component);
+        helper.checkIfIsObserved(component);
     },
 
     toggleObserved: function(component, event, helper){
-        let observeIcon = component.find('observeIcon');
-        let productId = component.get('v.product.Id');
-        let toastComponent = component.find('customToast');
-        let toastTitle;
-        let toastMsg;
-
-        if($A.util.hasClass(observeIcon, 'greyIcon')){
-            let action = component.get('c.insertToObserved');
-            action.setParams({
-                "productId": productId
-            });
-            $A.util.removeClass(observeIcon, 'greyIcon');
-            $A.util.addClass(observeIcon, 'highlightedIcon');
-            $A.enqueueAction(action);
-        }else{
-            $A.util.removeClass(observeIcon, 'highlightedIcon');
-            $A.util.addClass(observeIcon, 'greyIcon');
-        }
+        helper.doToggleObserved(component);
     },
 
     toggleBasket: function(component, event, helper){
-        let basketIcon = component.find('basketIcon');
-
-        if($A.util.hasClass(basketIcon, 'greyIcon')){
-            $A.util.removeClass(basketIcon, 'greyIcon');
-            $A.util.addClass(basketIcon, 'highlightedIcon');
-        }else{
-            $A.util.removeClass(basketIcon, 'highlightedIcon');
-            $A.util.addClass(basketIcon, 'greyIcon');
-        }
+        helper.doToggleBasket(component);
     }
 })
--- aura/FW_ProductView/FW_ProductViewHelper.js
+++ aura/FW_ProductView/FW_ProductViewHelper.js
@@ -1,5 +1,5 @@
 ({
-    returnNewestProducts: function(component, event, helper){
+    checkIfIsObserved: function(component, event, helper){
         let productId = component.get('v.product.Id');
         let observeIcon = component.find('observeIcon');
         let action = component.get('c.checkIfProductIsObserved');
@@ -13,5 +13,38 @@
             }
         });
         $A.enqueueAction(action);
+    },
+
+    doToggleObserved: function(component, event, helper){
+        let observeIcon = component.find('observeIcon');
+        let productId = component.get('v.product.Id');
+        let toastComponent = component.find('customToast');
+        let toastTitle;
+        let toastMsg;
+
+        if($A.util.hasClass(observeIcon, 'greyIcon')){
+            let action = component.get('c.insertToObserved');
+            action.setParams({
+                "productId": productId
+            });
+            $A.util.removeClass(observeIcon, 'greyIcon');
+            $A.util.addClass(observeIcon, 'highlightedIcon');
+            $A.enqueueAction(action);
+        }else{
+            $A.util.removeClass(observeIcon, 'highlightedIcon');
+            $A.util.addClass(observeIcon, 'greyIcon');
+        }
+    },
+
+    doToggleBasket: function(component, event, helper){
+        let basketIcon = component.find('basketIcon');
+
+        if($A.util.hasClass(basketIcon, 'greyIcon')){
+            $A.util.removeClass(basketIcon, 'greyIcon');
+            $A.util.addClass(basketIcon, 'highlightedIcon');
+        }else{
+            $A.util.removeClass(basketIcon, 'highlightedIcon');
+            $A.util.addClass(basketIcon, 'greyIcon');
+        }
     }
 })
