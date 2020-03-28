7bb1c8d61df225857c59b797eeffc7dde920853e
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-28 10:19:45 +0100

    added option of changing quantity in basket

--- aura/FW_ProductBasketResult/FW_ProductBasketResultController.js
+++ aura/FW_ProductBasketResult/FW_ProductBasketResultController.js
@@ -1,5 +1,17 @@
 ({
     deleteFromBasket: function(component, event, helper){
+        helper.doDeleteFromBasket(component);
+    },
 
+    decreaseQuantity: function(component, event, helper){
+        let quantityInputValue = component.get('v.quantity');
+        if(quantityInputValue > 0){
+            component.set('v.quantity', quantityInputValue-1);
+        }
+    },
+
+    increaseQuantity: function(component, event, helper){
+        let quantityInputValue = component.get('v.quantity');
+        component.set('v.quantity', quantityInputValue+1);
     }
 })
---
+++ aura/FW_ProductsBasket/FW_ProductsBasketHelper.js
@@ -0,0 +1,16 @@
+({
+    returnProductsFromBasket: function(component){
+        console.log('returnProductsFromBasket in helper');
+        let action = component.get("c.getProductsFromBasket");
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if(state === 'SUCCESS'){
+                component.set('v.results', response.getReturnValue());
+            }else{
+                let toastComponent = component.find('customToast');
+                toastComponent.showErrorToast(response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})
