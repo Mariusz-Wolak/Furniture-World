bc72e8cb966294a975034733a0f9b1ea98eb7776
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-29 09:02:43 +0200

    added calculating prices on results

--- aura/FW_ProductBasketResult/FW_ProductBasketResultController.js
+++ aura/FW_ProductBasketResult/FW_ProductBasketResultController.js
@@ -1,4 +1,8 @@
 ({
+    onInit: function(component, event, helper){
+        component.set('v.showTotalPrice', false);
+    },
+
     deleteFromBasket: function(component, event, helper){
         helper.doDeleteFromBasket(component);
     },
@@ -6,12 +10,52 @@
     decreaseQuantity: function(component, event, helper){
         let quantityInputValue = component.get('v.quantity');
         if(quantityInputValue > 0){
-            component.set('v.quantity', quantityInputValue-1);
+            if(quantityInputValue == 2){
+                component.set('v.showTotalPrice', false);
+            }
+            let totalPrice = component.get('v.totalPrice');
+            let productPrice = component.get('v.product.price');
+            let newQuantity = quantityInputValue-1;
+            component.set('v.quantity', newQuantity);
+
+            if(newQuantity == 0){
+                component.set('v.showTotalPrice', true);
+                totalPrice = 0;
+                component.set('v.totalPrice', totalPrice.toFixed(2));
+            }else{
+                totalPrice = (quantityInputValue-1)*productPrice;
+                component.set('v.totalPrice', totalPrice.toFixed(2));
+            }
         }
     },
 
     increaseQuantity: function(component, event, helper){
+        let totalPrice = component.get('v.totalPrice');
         let quantityInputValue = component.get('v.quantity');
-        component.set('v.quantity', quantityInputValue+1);
+        let productPrice = component.get('v.product.price');
+        let newQuantity = Number(quantityInputValue)+1;
+        component.set('v.quantity', newQuantity);
+
+        if(quantityInputValue > 0){
+            totalPrice = newQuantity * productPrice;
+            component.set('v.totalPrice', totalPrice.toFixed(2));
+            component.set('v.showTotalPrice', true);
+        }else{
+            component.set('v.showTotalPrice', false);
+        }
+    },
+
+    changeQuantity: function(component, event, helper){
+        let quantityInputValue = component.get('v.quantity');
+        let productPrice = component.get('v.product.price');
+
+        let totalPrice = quantityInputValue * productPrice;
+        component.set('v.totalPrice', totalPrice.toFixed(2));
+
+        if(quantityInputValue != 1){
+            component.set('v.showTotalPrice', true);
+        }else{
+            component.set('v.showTotalPrice', false);
+        }
     }
 })
