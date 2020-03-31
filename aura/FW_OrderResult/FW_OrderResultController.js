96db851961119b5376ea492c535b80fb3ecbe7de
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-31 09:26:57 +0200

    showing order details on incon click

--- aura/FW_OrderResult/FW_OrderResultController.js
+++ aura/FW_OrderResult/FW_OrderResultController.js
@@ -1,6 +1,14 @@
 ({
+    onInit: function(component, event, helper){
+        component.set('v.showOrderDetails', false);
+    },
+
     showDetails: function(component, event, helper){
         console.log('show details in result');
 
+        let order = component.get('v.order');
+        console.log('order: '+JSON.stringify(order));
+
+        component.set('v.showOrderDetails', true);
     }
 })
