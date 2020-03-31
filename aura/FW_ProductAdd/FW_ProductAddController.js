f077f1ce093f7b1840ac09c866077a86b3bade4f
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-03-31 22:28:43 +0200

    proper adding product

--- aura/FW_ProductAdd/FW_ProductAddController.js
+++ aura/FW_ProductAdd/FW_ProductAddController.js
@@ -3,9 +3,9 @@
         let initAction = component.get("c.getFamilyOptions");
         initAction.setCallback(this,function(response){
             if(response.getState() === "SUCCESS"){
-              component.set("v.families", response.getReturnValue());
+                component.set("v.families", response.getReturnValue());
             }else{
-
+                component.find('customToast').showErrorToast(response.getError());
             }
         });
         $A.enqueueAction(initAction);
@@ -18,19 +18,21 @@
             filesIds.push(item.documentId);
         });
         component.set("v.filesId", filesIds);
+        console.log('filesIds: '+component.get('v.filesId'));
     },
 
     createProduct : function(component, event){
+        let family = component.find('selectFamily').get('v.value');
+        console.log('family: '+family);
         let createAction = component.get("c.addProduct");
         createAction.setParams({
             "name" : component.get("v.name"),
             "description" : component.get("v.description"),
-            "family" : component.get("v.family"),
+            "family" : component.find("selectFamily").get("v.value"),
             "photosIds" : component.get("v.filesId")
         });
         console.log(JSON.stringify(createAction.getParams()));
         createAction.setCallback(this, function(response){
-            console.log(response.getState());
             if(response.getState() === "SUCCESS"){
                 let sendIdEvent = $A.get("e.c:FW_ProductIdSendToRecordView");
                 sendIdEvent.setParams({
@@ -38,7 +40,7 @@
                 });
                 sendIdEvent.fire();
             }else{
-                console.log(response.getError()[0]);
+                component.find('customToast').showErrorToast(response.getError());
             }
         });
         $A.enqueueAction(createAction);
