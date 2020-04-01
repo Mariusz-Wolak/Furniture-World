77676b7002a444d2a168422fe86913571db02ae4
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-04-01 15:39:05 +0200

    moved loading photos to helper

--- aura/FW_ProductPhotos/FW_ProductPhotosController.js
+++ aura/FW_ProductPhotos/FW_ProductPhotosController.js
@@ -1,38 +1,6 @@
 ({
     onInit: function(component, event, helper){
-        let loadPhotosAction = component.get('c.loadPhotos');
-        loadPhotosAction.setParams({
-           "id": component.get('v.recordId')
-        });
-        loadPhotosAction.setCallback(this, function(response){
-            let state = response.getState();
-            if(state === 'SUCCESS'){
-                let photosObjects = response.getReturnValue();
-                console.log('photosObject in response: '+JSON.stringify(photosObjects));
-                component.set('v.photosObjects', photosObjects);
-                for(let i=0; i<photosObjects.length; i++){
-                }
-            }else{
-                component.find('customToast').showErrorToast(response.getError());
-            }
-        });
-
-        let getMainPhotoAction = component.get('c.getMainPhoto');
-        getMainPhotoAction.setParams({
-           "productId": component.get('v.recordId')
-        });
-        getMainPhotoAction.setCallback(this, function(response){
-            let state = response.getState();
-            if(state === 'SUCCESS'){
-                console.log('response get main photo: '+JSON.stringify(response.getReturnValue()));
-                component.set('v.mainPhoto', response.getReturnValue());
-            }else{
-                component.find('customToast').showErrorToast(response.getError());
-            }
-        });
-
-        $A.enqueueAction(getMainPhotoAction);
-        $A.enqueueAction(loadPhotosAction);
+        helper.doLoadPhotos(component, event);
     },
 
     getPhotoFromEvent: function(component, event, helper){
