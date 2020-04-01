2e22348465070fde919f8cf439863810aabe53d9
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-04-01 15:12:37 +0200

    styled photos

--- aura/FW_ProductPhotos/FW_ProductPhotosController.js
+++ aura/FW_ProductPhotos/FW_ProductPhotosController.js
@@ -1,6 +1,5 @@
 ({
     onInit: function(component, event, helper){
-        console.log('ont init productPhotosCtrl');
         let loadPhotosAction = component.get('c.loadPhotos');
         loadPhotosAction.setParams({
            "id": component.get('v.recordId')
@@ -35,44 +34,6 @@
         $A.enqueueAction(getMainPhotoAction);
         $A.enqueueAction(loadPhotosAction);
     },
-
-//    setPhotos: function(component, event, helper){
-//        let productId = event.getParam('productId');
-//        component.set('v.productId', productId);
-//        let loadPhotosAction = component.get('c.loadPhotos');
-//        loadPhotosAction.setParams({
-//           "id": component.get('v.recordId')
-//        });
-//        loadPhotosAction.setCallback(this, function(response){
-//            let state = response.getState();
-//            if(state === 'SUCCESS'){
-//                let photosObjects = response.getReturnValue();
-//                component.set('v.photosObjects', photosObjects);
-//                for(let i=0; i<photosObjects.length; i++){
-//                   console.log('photo imgURL updated=: '+photosObjects[i].imgURL__c);
-//                }
-//            }else{
-//                component.find('customToast').showErrorToast(response.getError());
-//            }
-//        });
-//
-//        let getMainPhotoAction = component.get('c.getMainPhoto');
-//        getMainPhotoAction.setParams({
-//           "productId": component.get('v.recordId')
-//        });
-//        getMainPhotoAction.setCallback(this, function(response){
-//            let state = response.getState();
-//            if(state === 'SUCCESS'){
-//                console.log('response get main photo: '+JSON.stringify(response.getReturnValue()));
-//                component.set('v.mainPhoto', response.getReturnValue());
-//            }else{
-//                component.find('customToast').showErrorToast(response.getError());
-//            }
-//        });
-//
-//        $A.enqueueAction(getMainPhotoAction);
-//        $A.enqueueAction(action);
-//    },
 
     getPhotoFromEvent: function(component, event, helper){
         let mainPhoto = event.getParam('photo');
--- aura/FW_ProductSinglePhoto/FW_ProductSinglePhotoController.js
+++ aura/FW_ProductSinglePhoto/FW_ProductSinglePhotoController.js
@@ -1,8 +1,4 @@
 ({
-    onInit: function(component, event, helper){
-        console.log('on init single photo');
-    },
-
     sendMainPhoto: function(component, event, helper){
         helper.doSendMainPhoto(component, event);
     }
