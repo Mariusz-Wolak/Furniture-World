874fb99e967d2a826fd353ce630896e674d6d719
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-04-09 23:40:47 +0200

    removed possibility of deleting standrad pricebook

--- aura/FW_ProductsPricebookResult/FW_ProductsPricebookResultController.js
+++ aura/FW_ProductsPricebookResult/FW_ProductsPricebookResultController.js
@@ -1,4 +1,13 @@
 ({
+    onInit: function(component, event, helper){
+        let pricebook = component.get('v.pricebook');
+        if(pricebook.Name === 'Standard Price Book'){
+            component.set('v.isStandardPB', true);
+        }else{
+            component.set('v.isStandardPB', false);
+        }
+    },
+
     selectPricebook: function(component, event, helper){
         let pricebook = component.get('v.pricebook');
         let appEvent = $A.get('e.c:FW_SelectedPricebook');
