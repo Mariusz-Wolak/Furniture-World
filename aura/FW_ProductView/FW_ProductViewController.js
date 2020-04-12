dcd583b0568c78d479fecae7de9d712b1d39a1b1
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-04-12 19:34:40 +0200

    added quantity div

--- aura/FW_ProductView/FW_ProductViewController.js
+++ aura/FW_ProductView/FW_ProductViewController.js
@@ -3,6 +3,7 @@
         component.set('v.header', $A.get('$Label.c.Similar_Items'));
         component.set('v.isCommunity', true);
         component.set('v.productId', component.get('v.product.id'));
+        component.set('v.quantity', 1);
         helper.checkIfIsObserved(component);
         helper.returnNewestComments(component);
         helper.returnSimilarProducts(component);
@@ -50,5 +51,27 @@
     checkIfIsObserved: function(component, event, helper){
         component.set('v.productId', event.getParam('productId'));
         helper.checkIfIsObserved(component);
+    },
+
+    decreaseQuantity: function(component, event, helper){
+        let quantityInputValue = component.get('v.quantity');
+        if(quantityInputValue > 1){
+            let newQuantity = quantityInputValue-1;
+            component.set('v.quantity', newQuantity);
+        }
+    },
+
+    increaseQuantity: function(component, event, helper){
+        let quantityInputValue = component.get('v.quantity');
+        let newQuantity = Number(quantityInputValue)+1;
+        component.set('v.quantity', newQuantity);
+    },
+
+    changeQuantity: function(component, event, helper){
+        let quantityInputValue = component.get('v.quantity');
+        if(quantityInputValue == ''){
+            quantityInputValue = 1;
+            component.set('v.quantity', quantityInputValue);
+        }
     }
 })
