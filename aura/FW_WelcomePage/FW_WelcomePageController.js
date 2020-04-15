85525669ec959a8e13d1360b0ee088f9da6a2563
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-04-15 09:05:58 +0200

    going to search on div click

--- aura/FW_WelcomePage/FW_WelcomePageController.js
+++ aura/FW_WelcomePage/FW_WelcomePageController.js
@@ -1,3 +1,5 @@
 ({
-
+    goToSearch: function(component, event, helper){
+        window.open('https://fw-community-developer-edition.eu32.force.com/furnitureworldcommunity/s/Search-Furniture', '_top');
+    }
 })
