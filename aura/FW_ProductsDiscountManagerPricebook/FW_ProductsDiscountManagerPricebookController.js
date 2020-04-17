402da961e920d7cee3da240affe3249793b0b0e2
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-04-17 12:00:51 +0200

    added searching possibility from any tab

--- aura/FW_ProductsDiscountManagerPricebook/FW_ProductsDiscountManagerPricebookController.js
+++ aura/FW_ProductsDiscountManagerPricebook/FW_ProductsDiscountManagerPricebookController.js
@@ -5,10 +5,6 @@
         component.set('v.endDate', null);
     },
 
-    receiveSearchResults: function(component, event, helper){
-        component.set("v.results", event.getParam("results"));
-    },
-
     closeModal: function(component, event, helper){
         component.set('v.showModal', false);
         component.set('v.name', null);
--- aura/FW_ProductsSearchResults/FW_ProductsSearchResultsController.js
+++ aura/FW_ProductsSearchResults/FW_ProductsSearchResultsController.js
@@ -6,24 +6,34 @@
         meta.setAttribute("content", "date=no;img-src 'self' data:");
         document.getElementsByTagName('head')[0].appendChild(meta);
 
-        helper.returnNewestProducts(component);
-
-        component.set('v.header', $A.get('$Label.c.Newest_Items'));
+        let productsUrl = decodeURIComponent(window.location.search.substring(1));
+        if(productsUrl != null && productsUrl != undefined && productsUrl != ''){
+            let variables = productsUrl.split('&');
+            let searchItem;
+            for (let i = 0; i<variables.length;i++){
+                if(variables[i].split('=')[0] ='searchItem'){
+                    searchItem = variables[i].split('=')[1].replace('+', ' ');
+                }
+            }
+            let action = component.get("c.getSearchResults");
+            action.setParams({
+                "name": searchItem
+            });
+            action.setCallback(this, function(response){
+                let state = response.getState();
+                if(state === 'SUCCESS'){
+                    component.set("v.results", response.getReturnValue());
+                    component.set("v.header", $A.get("$Label.c.Showing_Results_For") + ": " + searchItem);
+            }else{
+                component.find('customToast').showErrorToast(response.getError());
+            }
+        });
+        $A.enqueueAction(action);
+        }else{
+            helper.returnNewestProducts(component);
+            component.set('v.header', $A.get('$Label.c.Newest_Items'));
+        }
         component.set('v.showItems', true);
-    },
-
-    receiveSearchResults: function(component, event, helper){
-        let searchItemName = event.getParam("searchItemName");
-        let resultsFromEvent = event.getParam("results");
-
-
-        component.set("v.results", resultsFromEvent);
-        component.set("v.header", $A.get("$Label.c.Showing_Results_For") + ": " + searchItemName);
-
-        component.set('v.showItems', true);
-        component.set('v.showProductView', false);
-        component.set('v.showHeader', false);
-
     },
 
     showProduct: function(component, event, helper){
