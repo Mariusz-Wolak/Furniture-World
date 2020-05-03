a463eeb18e1ae8dc9b24360d2b1b6d25ddfcbb2e
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-05-03 14:57:09 +0200

    added label to modal

--- src/aura/FW_PricebookUpdateModal/FW_PricebookUpdateModalHelper.js
+++ src/aura/FW_PricebookUpdateModal/FW_PricebookUpdateModalHelper.js
@@ -8,7 +8,7 @@
         action.setCallback(this, function(response){
             let state = response.getState();
             if(state === 'SUCCESS'){
-                component.find('customToast').showSuccessToast('Price book has been updated successfully');
+                component.find('customToast').showSuccessToast($A.get("$Label.c.Pricebook_Has_Been_Updated_Successfully"));
                 component.set('v.showPricebookDetailsModal', false);
                 let updatedPricebook = response.getReturnValue();
                 let sendPricebookEvent = $A.get('e.c:FW_SendUpdatedPricebook');
