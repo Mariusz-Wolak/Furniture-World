e372ef210f9d99db65202e2695329a0fff3718d8
Mariusz Wolak <mariusz.wolak@yahoo.com>
2020-05-03 19:20:06 +0200

    deleted console log

--- src/aura/FW_PricebookAddModal/FW_PricebookAddModalController.js
+++ src/aura/FW_PricebookAddModal/FW_PricebookAddModalController.js
@@ -6,7 +6,6 @@
     },
 
     closeModal: function(component, event, helper){
-        console.log('close add pricebook modal 18:56');
         component.set('v.showModal', false);
         component.set('v.name', null);
         component.set('v.startDate', null);
--- src/aura/FW_PricebookSelected/FW_PricebookSelectedController.js
+++ src/aura/FW_PricebookSelected/FW_PricebookSelectedController.js
@@ -1,7 +1,6 @@
 ({
     onInit: function(component, event, helper){
         helper.receivePricebooksList(component, event);
-        console.log('after receiving pricebooks list 19:17');
     },
 
     receiveSearchResults: function(component, event, helper){
